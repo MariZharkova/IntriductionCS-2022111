6dacca4ea56d0b37f7987cd7d4d4cdc4b65aed00
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 01:47:59 +0400

    add 05 project

---
+++ 05/Program.cs
@@ -0,0 +1,23 @@
+﻿// 5. С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+
+Console.Write("Введите число a: ");
+double a=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число b: ");
+double b=Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число c: ");
+double c=Convert.ToDouble(Console.ReadLine());
+double max;
+max = a;
+
+if (max < b)
+{
+    max = b;
+} 
+else
+{
+   if (max < c)
+   {
+    max = c;
+   }
+}
+Console.WriteLine($"Максимальное значение: {max}");

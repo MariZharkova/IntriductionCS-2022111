c62d49b3c8251b4c1ba1894ad4b38249f5f10f96
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 02:38:12 +0400

    add 09 project

---
+++ 09/Program.cs
@@ -0,0 +1,18 @@
+﻿//9. Вывести на экран четные числа от 1 до N
+
+Console.Write("Число N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=2;
+while(i<=N)
+{
+    Console.Write($"{i} ");
+    i=i+2;
+}
+Console.WriteLine();
+for(int j=1;j<=N;j=j+1)
+{
+    if (j%2==0)
+    {
+        Console.Write($"{j} ");
+    }
+}

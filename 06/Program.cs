0d51a8c8fbdfa780956041ee8851d0eac0f186fb
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 02:04:20 +0400

    add 06 project

---
+++ 06/Program.cs
@@ -0,0 +1,6 @@
+﻿// 6. Написать программу вычисления значения функции y = sin(a). (Класс Math). 
+
+Console.Write("Введите угол a: ");
+double a=Convert.ToDouble(Console.ReadLine());
+double y=Math.Sin(a);
+Console.WriteLine($"y = {y}");

e2cd241ead8f90af127e678b3144afbe2d383717
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 02:44:27 +0400

    add 10 project

---
+++ 10/Program.cs
@@ -0,0 +1,10 @@
+﻿// 10. Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
+
+Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int last;
+
+last = N % 10;
+{
+   Console.Write($"{last} ");
+}

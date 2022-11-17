76daef85c2be18cb45521d6ed7d09799ebb86cd7
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 03:07:06 +0400

    add 13 project

---
+++ 13/Program.cs
@@ -0,0 +1,8 @@
+﻿// 13. Удалить вторую цифру целого числа введенного с клавиатуры.
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+//345=35
+//12345=1235
+int b=a/100*10+a%10;
+Console.WriteLine(b);

812e5cf212064ebd50dd346a6bcdf9622c4b5511
Marina Zharkova <muzharkova@gmail.com>
2022-11-18 13:50:39 +0400

    add 15 project

---
+++ 15/Program.cs
@@ -0,0 +1,22 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+
+Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=0;
+int res;
+int a = N;
+
+while (a != 0)
+{
+    a = a/10;
+    i = i + 1;
+}
+if (i <= 2)
+{
+    Console.Write("NO");
+}
+else
+{
+    res = (N/(Convert.ToInt32(Math.Pow(10,(i-3)))))%10;
+    Console.Write($"Третья цифра: {res}");
+}
